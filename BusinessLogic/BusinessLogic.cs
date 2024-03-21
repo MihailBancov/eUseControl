0550fdc5e9d0986d216074e5957e04a5e14f2382
Yury Turlakov <112777014+Bledny@users.noreply.github.com>
2024-03-21 11:49:43 +0200

    Add files via upload

---
+++ BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using eUseControl.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public ISession GetSessionBL()
+        { 
+        return new SessionBL ();
+            
+        } 
+    }
+ }
---
+++ BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.User
+{
+    class UDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
