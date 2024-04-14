0d4fad8560ec91b9ab523f5435bad9908d6d1028
Миша Банков <mihail.bancov@isa.utm.md>
2024-04-14 20:00:19 +0300

    14

--- eUseControl.Web/Models/VehicleData.cs
+++ eUseControl.Web/Models/VehicleData.cs
@@ -7,5 +7,21 @@ namespace eUseControl.Models
 {
     public class VehicleData
     {
+        public string name { get; set; }
+        public int price { get; set; }
+        public int Power { get; set; }
+        public int torque { get; set; }
+        public int weight { get; set; }
+        public string image { get; set; }
+
+        public VehicleData()
+        {
+            name = null;
+            price = 0;
+            Power = 0;
+            torque = 0;
+            weight = 0;
+            image = null;
+        }
     }
 }
